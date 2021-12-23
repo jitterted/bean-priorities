359766ebff74606a6a554598351def886f5089bc
Daniel Ranner <daniel.ranner@jollydays.com>
2021-12-24 00:45:40 +0100

    Resolving also works when disabling the JPA adapter via an inverted @ConditionalOnProperty

--- src/main/java/ninja/ranner/beanpriorities/BeanPrioritiesApplication.java
+++ src/main/java/ninja/ranner/beanpriorities/BeanPrioritiesApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-import javax.annotation.Priority;
 
 @SpringBootApplication
 public class BeanPrioritiesApplication {
@@ -16,7 +13,6 @@ public class BeanPrioritiesApplication {
         SpringApplication.run(BeanPrioritiesApplication.class, args);
     }
 
-    @Primary
     @Bean
     @ConditionalOnProperty("demo.repository.dummy")
     FooRepository dummyRepository() {
--- src/main/java/ninja/ranner/beanpriorities/adapter/jpa/FooJpaRepositoryAdapter.java
+++ src/main/java/ninja/ranner/beanpriorities/adapter/jpa/FooJpaRepositoryAdapter.java
@@ -3,9 +3,11 @@ package ninja.ranner.beanpriorities.adapter.jpa;
 import ninja.ranner.beanpriorities.application.FooEntity;
 import ninja.ranner.beanpriorities.application.FooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(value = "demo.repository.dummy", havingValue = "false", matchIfMissing = true)
 public class FooJpaRepositoryAdapter implements FooRepository {
     private final FooJpaRepository jpaRepository;
 
